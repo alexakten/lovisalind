7495ac1c643062e51582762ea7a91c0ba2aac41b
Alex <alex.akten@outlook.com>
2023-12-29 10:43:07 -0300

    line break

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="flex items-center h-screen bg-blue-600 justify-center px-2">
       <h1 className="text-3xl text-white">
         Hej hjärtat! Det här är din egna domän där du kan bygga din portfolio!
+        <br />
         Tänkte att det skulle passa nu när du blev intresserad av att frilansa o
         så! Puss min fina, älskar dig! ❤️
       </h1>
