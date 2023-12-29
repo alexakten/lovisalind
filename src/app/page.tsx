9c2ced1d1d36f6905cb06d8d4983a941fc66f6cb
Alex <alex.akten@outlook.com>
2023-12-29 10:40:56 -0300

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <main className="flex items-center h-screen bg-blue-600 justify-center">
       <h1 className="text-3xl text-white">
-        Hej älskling! Det här är din egna domän där du kan ha dina projekt!
-        Puss o kram!
+        Hej hjärtat! Det här är din egna domän där du kan bygga din portfolio!
+        Tänkte att det skulle passa nu när du blev intresserad av att frilansa o
+        så! Puss min fina, älskar dig! ❤️
       </h1>
     </main>
   );
