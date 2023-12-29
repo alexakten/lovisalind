8ab35b9840b69048d84297259a1044e714a65578
Alex <alex.akten@outlook.com>
2023-12-29 10:39:11 -0300

    fullscreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex items-center bg-blue-600 justify-center">
+    <main className="flex items-center h-screen bg-blue-600 justify-center">
       <h1 className="text-3xl text-white">
         Hej älskling! Det här är din egna domän där du kan ha dina projekt!
         Puss o kram!
