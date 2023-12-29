20df136893ff25c3cc39cb678e7320ca93ff4feb
Alex <alex.akten@outlook.com>
2023-12-29 10:36:06 -0300

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex items-center bg-blue-600 justify-center">
+      <h1 className="text-3xl text-white">
+        Hej älskling! Det här är din egna domän där du kan ha dina projekt!
+        Puss!
+      </h1>
+    </main>
+  );
 }
