822f6efbf5c099cf292b623f3c70f3fcf8390945
Alex <alex.akten@outlook.com>
2023-12-29 10:44:10 -0300

    height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex items-center h-screen bg-blue-600 justify-center px-2">
+    <main
+      className="flex items-center bg-blue-600 justify-center px-2"
+      style={{ height: "100svh" }}
+    >
       <h1 className="text-3xl text-white">
         Hej hjärtat! Det här är din egna domän där du kan bygga din portfolio!
         <br />
