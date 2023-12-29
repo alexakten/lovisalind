de5f7fedf326a2c74ab10b4ead692f8f1766164b
Alex <alex.akten@outlook.com>
2023-12-29 10:42:20 -0300

    padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex items-center h-screen bg-blue-600 justify-center">
+    <main className="flex items-center h-screen bg-blue-600 justify-center px-2">
       <h1 className="text-3xl text-white">
         Hej hjärtat! Det här är din egna domän där du kan bygga din portfolio!
         Tänkte att det skulle passa nu när du blev intresserad av att frilansa o
